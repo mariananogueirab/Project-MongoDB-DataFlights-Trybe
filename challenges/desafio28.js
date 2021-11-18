ca2c64fe0ee6daa94a1ecffe2ddd4729caa698f5
mariananbp <mariananbp@gmail.com>
2021-11-18 00:39:01 -0300

    lint

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,13 @@
+const LATAM = "LATAM AIRLINES BRASIL";
+
 db.resumoVoos.insertOne({
-  empresa: "LATAM AIRLINES BRASIL",
+  empresa: LATAM,
   totalVoosDomesticos: db.voos.find({
     "empresa.nome": "LATAM AIRLINES BRASIL",
   natureza: "Doméstica" }).count(),
 });
 
-db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
+db.resumoVoos.findOne(
+  { empresa: LATAM },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+);
